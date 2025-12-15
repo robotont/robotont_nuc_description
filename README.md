44175f004963c865e4c992f7f2fe4802969f9f78
miriam <calafa@ut.ee>
2025-12-15 15:41:14 +0200

    add color parameters to launch

--- README.md
+++ README.md
@@ -55,11 +55,15 @@ Supported parameters:
 Displays the robot's model in rviz, starts joint_state_publisher and robot_state_publisher
 ```bash
 #### Load generation 3 model (can also specify 'generation:=3')
-ros2 launch robotont_description display_simulated_robot.launch.py
+ros2 launch robotont_description display_robot_model.launch.py
+```
+```bash
+#### Load generation 3 model with specific colors (blue, light_blue, purple, yellow, green, dark_green, black, gray)
+ros2 launch robotont_nuc_description display_robot_model.launch.py primary_color:=light_blue secondary_color:=purple
 ```
 ```bash
 #### Load generation 2.1 model
-ros2 launch robotont_description display_simulated_robot.launch.py generation:=2.1
+ros2 launch robotont_description display_robot_model.launch.py generation:=2.1
 ```
 #### 2.2. Description
 Starts joint_state_publisher and robot_state_publisher, robot model is published on /robot_description topic.
--- launch/display_robot_model.launch.py
+++ launch/display_robot_model.launch.py
@@ -15,18 +15,61 @@ def generate_launch_description():
     generation_decl = DeclareLaunchArgument(name='generation', default_value='3')
     model_decl = DeclareLaunchArgument(name='model', default_value='')
 
+    primary_color_decl = DeclareLaunchArgument(
+        name='primary_color',
+        default_value='light_blue'   
+    )
+    secondary_color_decl = DeclareLaunchArgument(
+        name='secondary_color',
+        default_value='gray'   
+    )
+
     model_arg = LaunchConfiguration('model')
     rviz_config_arg = LaunchConfiguration('rviz_config')
     rviz_fixed_frame_arg = LaunchConfiguration('rviz_fixed_frame')
     generation_arg = LaunchConfiguration('generation')
+
+    primary_color_name = LaunchConfiguration('primary_color')
+    secondary_color_name = LaunchConfiguration('secondary_color')
 
     robot_model_path = PythonExpression([
         '"" if "', model_arg,
         '" else "', str(package / "urdf/"), '" + (',
         '"/gen2_1/robotont_realsense.urdf.xacro" if "', generation_arg, '" == "2.1" else "/gen3/robotont_realsense.urdf.xacro")'
     ])
+    primary_color_rgba = PythonExpression([
+        '"0.16 0.65 0.98 1.0" if "', primary_color_name, '" == "light_blue" else ',
+        '"0.00 0.35 0.90 1.0" if "', primary_color_name, '" == "blue" else ',
+        '"1.00 1.00 0.00 1.0" if "', primary_color_name, '" == "yellow" else ',
+        '"0.10 0.10 0.10 1.0" if "', primary_color_name, '" == "black" else ',
+        '"0.45 0.20 0.65 1.0" if "', primary_color_name, '" == "purple" else ',
+        '"0.75 0.75 0.75 1.0" if "', primary_color_name, '" == "gray" else ',
+        '"0.00 0.45 0.25 1.0" if "', primary_color_name, '" == "dark_green" else ',
+        '"0.00 0.80 0.30 1.0" if "', primary_color_name, '" == "green" else ',
+        '"0.16 0.65 0.98 1.0"'
+    ])
 
-    robot_description = ParameterValue(Command(['xacro ', robot_model_path]), value_type=str)
+    secondary_color_rgba = PythonExpression([
+        '"0.16 0.65 0.98 1.0" if "', secondary_color_name, '" == "light_blue" else ',
+        '"0.00 0.35 0.90 1.0" if "', secondary_color_name, '" == "blue" else ',
+        '"1.00 1.00 0.00 1.0" if "', secondary_color_name, '" == "yellow" else ',
+        '"0.10 0.10 0.10 1.0" if "', secondary_color_name, '" == "black" else ',
+        '"0.75 0.75 0.75 1.0" if "', secondary_color_name, '" == "gray" else ',
+        '"0.45 0.20 0.65 1.0" if "', secondary_color_name, '" == "purple" else ',
+        '"0.00 0.45 0.25 1.0" if "', secondary_color_name, '" == "dark_green" else ',
+        '"0.00 0.80 0.30 1.0" if "', secondary_color_name, '" == "green" else ',
+        '"0.75 0.75 0.75 1.0"'
+    ])
+
+    robot_description = ParameterValue(
+        Command([
+            'xacro ',
+            robot_model_path,
+            ' main_color:="', primary_color_rgba, '"',
+            ' second_color:="', secondary_color_rgba, '"'
+        ]),
+        value_type=str
+    )
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -57,6 +100,8 @@ def generate_launch_description():
             generation_decl,
             rviz_config_decl,
             rviz_fixed_frame_decl,
+            primary_color_decl,
+            secondary_color_decl,
             model_decl,
             joint_state_publisher_node,
             robot_state_publisher_node,
